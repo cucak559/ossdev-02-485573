abefee23817032c8777becb0206ca6002b07dcbe
Michal Čaniga <michalcaniga559@gmail.com>
2020-04-11 21:13:48 +0200

    Tests for sieve implemented

---
+++ sieve/tests/sieve_test.py
@@ -0,0 +1,28 @@
+import unittest
+import sys
+
+sys.path.insert(0, '../')
+from sieve import sieve_in_interval, calculate_primes_in_interval
+
+
+class SieveTest(unittest.TestCase):
+    def test_valid_values(self):
+        res = sieve_in_interval(2, 2)
+        self.assertEqual(res, [2])
+
+        res = sieve_in_interval(2, 30)
+        self.assertEqual(res, [2, 3, 5, 7, 11, 13, 17, 19, 23, 29])
+
+        res = sieve_in_interval(58, 114)
+        self.assertEqual(res, ([59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113]))
+
+    def test_invalid_values(self):
+        with self.assertRaises(ValueError):
+            sieve_in_interval(-20, 20)
+
+        with self.assertRaises(ValueError):
+            sieve_in_interval(80, 30)
+
+
+if __name__ == '__main__':
+    unittest.main()
